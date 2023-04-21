2901f9da78c6c72489dfd0e77771cbc62e6c7299
Pritam <itspritammazumder@gmail.com>
2023-04-21 16:29:30 +0530

    checkpoint 8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import {CreateContainer, Header, MainContainer} from "./components";
-import {Route, Routes} from "react-router-dom";
-import {AnimatePresence} from "framer-motion";
-
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { CreateContainer, Header, MainContainer } from "./components";
 
 const App = () => {
   return (
-      <AnimatePresence mode="wait">
-          <div className=" w-screen h-auto flex flex-col bg-primary">
-              <Header />
+    <AnimatePresence mode="wait">
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
 
-              <main className='mt-24 p-8 w-full'>
-                  <Routes>
-                      <Route path='/*' element={<MainContainer />} />
-                      <Route path='/createItem' element={<CreateContainer />} />
-                  </Routes>
-              </main>
-          </div>
-      </AnimatePresence>
-
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 };
 
